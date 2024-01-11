cf688c6574f08a23a430c743c054641cb4f20f62
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2024-01-11 21:25:11 +0530

    day 11 push

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./_Components/Header";
+import Hero from "./_Components/Hero";
 
 const page = () => {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 };
