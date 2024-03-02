16f673aa453d7ea014d4ffe964d1b44635629e97
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2024-03-02 15:45:24 +0530

    side navbar style and active.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,60 +1,62 @@
 "use client";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, SignOutButton } from "@clerk/nextjs";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import * as Tabs from "@radix-ui/react-tabs";
 import { TbLogout } from "react-icons/tb";
-import { RiArchiveDrawerFill, RiArchiveFill } from "react-icons/ri";
+import { LuHardDriveUpload } from "react-icons/lu";
+import { LuFileStack } from "react-icons/lu";
+import { GrUpgrade } from "react-icons/gr";
+import { VscSignOut } from "react-icons/vsc";
+
 const Sidebar = () => {
   const tabItems = [
     {
-      icon: <RiArchiveFill />,
-      name: "Overview",
+      icon: <LuHardDriveUpload />,
+      name: "Upload",
     },
     {
-      icon: <RiArchiveFill />,
-      name: "Integration",
+      icon: <LuFileStack />,
+      name: "Files",
     },
     {
-      icon: <RiArchiveFill />,
-      name: "Billing",
-    },
-    {
-      icon: <RiArchiveFill />,
-      name: "Transactions",
-    },
-    {
-      icon: <RiArchiveFill />,
-      name: "plans",
+      icon: <GrUpgrade />,
+      name: "Upgrad",
     },
   ];
   return (
     <>
-      <Tabs.Root
-        className="max-w-screen-xl mx-auto px-4 md:px-8"
-        defaultValue="Overview"
-      >
+      <Tabs.Root className="w-full mx-auto p-2" defaultValue="Overview">
         <Tabs.List
-          className="w-full border-b flex items-center gap-x-3 overflow-x-auto text-sm"
+          className="w-full border-b flex justify-between items-center gap-x-3 overflow-x-auto text-sm px-4"
           aria-label="Manage your own account"
         >
-          {tabItems.map((item, idx) => (
-            <Tabs.Trigger
-              key={idx}
-              className="group outline-none py-1.5 border-b-2 border-white text-gray-500 data-[state=active]:border-indigo-600 data-[state=active]:text-indigo-600"
-              value={item.name}
-            >
-              <div className="flex items-center gap-x-2 py-1.5 px-3 rounded-lg duration-150 group-hover:text-indigo-600 group-hover:bg-gray-50 group-active:bg-gray-100 font-medium">
-                {item.icon}
-                {item.name}
-              </div>
-            </Tabs.Trigger>
-          ))}
+          <UserButton />
+          <div>
+            {tabItems.map((item, idx) => (
+              <Tabs.Trigger
+                key={idx}
+                className="group outline-none py-1.5 border-b-2 border-white text-gray-500 data-[state=active]:border-indigo-600 data-[state=active]:text-indigo-600"
+                value={item.name}
+              >
+                <div className="flex items-center gap-x-2 py-1.5 px-3 rounded-lg duration-150 group-hover:text-indigo-600 group-hover:bg-gray-50 group-active:bg-gray-100 font-medium">
+                  {item.icon}
+                  {item.name}
+                </div>
+              </Tabs.Trigger>
+            ))}
+          </div>
+          <SignOutButton>
+            <button className="flex" title="Sign out">
+              <VscSignOut className="outline-none text-gray-500 hover:text-indigo-600 hover:bg-gray-50 active:bg-gray-100 font-medium scale-125" />
+            </button>
+          </SignOutButton>
         </Tabs.List>
         {tabItems.map((item, idx) => (
           <Tabs.Content key={idx} className="py-6" value={item.name}>
             <p className="text-xs leading-normal">
-              This is <b>{item.name}</b> Tab containing the items to put in here.
+              This is <b>{item.name}</b> Tab containing the items to put in
+              here.
             </p>
           </Tabs.Content>
         ))}
