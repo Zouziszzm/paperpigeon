d70319d409cb365f80f9250cfb45dbc336af30d9
Zouziszzm <uaepeekawariour@gmail.com>
2024-03-14 23:27:31 +0530

    this is the progress

---
+++ src/app/_components/Header.jsx
@@ -0,0 +1,114 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+const Header = () => {
+  const [menu, setMenu] = useState(false);
+  const handelmenu = () => {
+    if (!menu) {
+      setMenu(true);
+    } else {
+      setMenu(false);
+    }
+  };
+  return (
+    <header className="bg-white border-b-gray-200/50 border-[1px]">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex-1 md:flex md:items-center md:gap-12">
+            <a className="block text-teal-600" href="#">
+              <span className="sr-only">Home</span>
+              <Image
+                className=""
+                src="/Origami_b.svg"
+                height={45}
+                width={45}
+                alt="Logo"
+              ></Image>
+            </a>
+          </div>
+          <div className="md:flex md:items-center md:gap-12">
+            <nav aria-label="Global" className="hidden md:block">
+              <ul className="flex items-center gap-6 text-sm">
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="#"
+                  >
+                    {" "}
+                    Home{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="#"
+                  >
+                    {" "}
+                    About{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="#"
+                  >
+                    {" "}
+                    Contact Us{" "}
+                  </a>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="flex items-center gap-4">
+              <div className="sm:flex sm:gap-4">
+                <div className="sm:flex">
+                  <a
+                    className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+                    href="#"
+                  >
+                    Get Started
+                  </a>
+                </div>
+              </div>
+
+              <div className="block relative md:hidden">
+                <button
+                  className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
+                  onClick={handelmenu}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+                <div className="absolute right-0 top-14">
+                  <div
+                    className={`bg-gray-200 h-[150px] w-[150px] ${!menu ? "block" : "hidden"}`}
+                  >
+                    <ul>
+                      <li></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
     <>
-      <div></div>
+      <Header/>
+      <div>body</div>
     </>
   );
 }
